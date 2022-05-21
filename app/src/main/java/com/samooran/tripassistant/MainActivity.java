0a76e9621f6ac6cc5fa821aaa178e4e48272fa8a
smoort <sarav_m@hotmail.com>
2022-05-22 01:18:15 +0530

    first commit

---
+++ app/src/main/java/com/samooran/tripassistant/MainActivity.java
@@ -0,0 +1,14 @@
+package com.samooran.tripassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
